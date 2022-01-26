08938181cf9bd19c3bd6fe739ffaab3e72b451a3
a.savanovich <asavanster@gmail.com>
2022-01-26 04:15:27 +0300

    improve sw

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const version = "0.0.1";
-const CACHE = 'cache-only';
+const version = "0.0.2";
+const CACHE = 'cache-only-' + version;
 
 function fromCache(request) {
     return caches.open(CACHE).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            return matching || Promise.reject('no-match');
+        return cache.match(request, {ignoreSearch: true}).then(function (matching) {
+            return matching || Promise.reject('request-not-in-cache');
         });
     });
 }
@@ -16,21 +16,44 @@ function precache() {
             './images/male.svg',
             './images/female.svg',
             './images/they.svg',
-			'./images/ment.svg',
-			'./images/ment_192.png',
-			'./images/ment_512.png'
+            './images/ment.svg',
+            './images/ment_192.png',
+            './images/ment_512.png'
         ]);
     });
 }
 
 self.addEventListener('install', function (evt) {
-    evt.waitUntil(precache());
+    evt.waitUntil(precache().then(function () {
+        return self.skipWaiting();
+    }));
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return fromCache(event.request);
+function networkOrCache(request) {
+    return fetch(request).then(function (response) {
+        return response.ok ? response : fromCache(request);
+    })
+        .catch(function () {
+            return fromCache(request);
+        });
+}
+
+self.addEventListener('fetch', function (evt) {
+    evt.respondWith(networkOrCache(evt.request));
+});
+
+self.addEventListener('activate', function (evt) {
+    evt.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(function () {
+            return self.clients.claim();
         })
     );
 });
